beb89237947ea9110c441977cf30156b69a6dc09
teskew <teskinder1@gmail.com>
2020-11-19 13:28:03 -0600

    commit tree 510971f1db138807ff9846f8663e48553978a87e

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,16 @@
-def hello_t
+def hello_t(names)
+  if block_given?
+    i = 0
+    while i < names.length
+      yield(names[i])
+      i = i + 1
+   end
+   names
+  else
+
+    puts "Hey! No block was given!"
 
 end
+end
 
-# call your method here!
 
